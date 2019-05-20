5b0010b693d7350e3289826c22c30c12227d7cd4
Влад Каченя <v.s.kachenya@gmail.com>
2019-05-20 17:19:17 +0300

    some corrections

---
+++ src/GoodsStore.Web/GoodsStore.Web.Presentation/Keys/ParametrKeys.cs
@@ -0,0 +1,10 @@
+﻿namespace GoodsStore.Web.Presentation.Keys
+{
+    public static class ParametrKeys
+    {
+        public const string parametr = nameof(parametr);
+        public const string phrase_parametr = nameof(phrase_parametr);
+        public const string RangeParametr = nameof(parametr);
+
+    }
+}
